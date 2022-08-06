2e820647827d6cc4f286eefb4d4d0f512e6537d9
Jiann <72090089+Jiann-Ma@users.noreply.github.com>
2022-08-07 00:04:39 +0800

    Update Register-ScheduledTask.ps1

--- Register-ScheduledTask.ps1
+++ Register-ScheduledTask.ps1
@@ -3,5 +3,6 @@ $Trigger= New-ScheduledTaskTrigger -Once -At (get-date) -RepetitionInterval (New
 #用甚麼身分執行，這邊是抓當前登入電腦的身分。
 $ComputerUser= [System.Security.Principal.WindowsIdentity]::GetCurrent().Name
 #要注意這邊的各種參數，比較重要的就是ExecutionPolicy這一塊，因為微軟預設是鎖住Powershell script的，要額外開啟。
+#另外，這邊的路徑是放在桌面上，如果要改放別的位置，記得修改路徑。
 $Action= New-ScheduledTaskAction -Execute "Powershell.exe" -Argument "-NoProfile -NoLogo -NonInteractive -ExecutionPolicy Bypass C:\Users\$env:UserName\Desktop\MiningCheck.ps1"
 Register-ScheduledTask -TaskName "Mining_Check" -Trigger $Trigger -User $ComputerUser -Action $Action -RunLevel Highest –Force
