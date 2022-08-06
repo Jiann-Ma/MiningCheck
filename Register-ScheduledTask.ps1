06d5d0c89e90b1d5ce48a28ef0c8f7ecc42eddc1
Jiann <72090089+Jiann-Ma@users.noreply.github.com>
2022-08-06 20:52:21 +0800

    Add files via upload

---
+++ Register-ScheduledTask.ps1
@@ -0,0 +1,4 @@
+﻿$Trigger= New-ScheduledTaskTrigger -Once -At (get-date) -RepetitionInterval (New-TimeSpan -Minutes 30)
+$ComputerUser= [System.Security.Principal.WindowsIdentity]::GetCurrent().Name
+$Action= New-ScheduledTaskAction -Execute "Powershell.exe" -Argument "-NoProfile -NoLogo -NonInteractive -ExecutionPolicy Bypass C:\Users\$env:UserName\Desktop\miningCheck\MiningCheck.ps1"
+Register-ScheduledTask -TaskName "Mining_Check" -Trigger $Trigger -User $ComputerUser -Action $Action -RunLevel Highest –Force
